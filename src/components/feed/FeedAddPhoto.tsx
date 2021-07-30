bda5f112f617f311b99004305ece92937e62ce3f
gudwh14 <alxious@naver.com>
2021-07-30 16:02:37 +0900

    feat : #9 피드 작성페이지 사진 넣기 컴포넌트 UI 구성
    피드 작성페이지에 사용되는 사진 넣기 항목을 컴포넌트로 구성하여 UI 제작
    
    ISSUE#9

---
+++ src/components/feed/FeedAddPhoto.tsx
@@ -0,0 +1,50 @@
+import style from './feedAddPhoto.module.css'
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import {ChangeEvent, useState} from "react";
+
+const FeedAddPhoto = () : JSX.Element => {
+    const [imgFile, setImgFile] = useState<Array<File>>([]);
+    const [imgFileUrl, setImgFileUrl] = useState<Array<string>>([]);
+
+    const onChangeImage = (event : ChangeEvent) => {
+        const input = event.target as HTMLInputElement;
+        if(!input.files?.length) { // 파일이 들어오지 않았을때 종료
+            return;
+        }
+        if(imgFile.length>=6) {
+            return;
+        }
+        const file : File = input.files[0]; // file 추출하기
+        const fileUrl = URL.createObjectURL(file); // file 로 URL 만들기
+
+        setImgFile(imgFile.concat(file));
+        setImgFileUrl(imgFileUrl.concat(fileUrl));
+    }
+
+    const renderInputImg = imgFileUrl.map((imgUrl,index)=> {
+        return (
+            <div key={imgUrl} className={style.img_sub_box}>
+                <img src={imgUrl} className={style.img}/>
+                <RemoveCircleIcon className={style.img_delete_btn} color="error" fontSize={"small"}/>
+            </div>
+        )
+    })
+
+    return (
+        <div className={style.container}>
+            <div className={style.title_box}>
+                <span className={style.main_txt}>사진 넣기</span>
+                <span className={style.sub_txt}>최대 5장 까지 사진을 넣을 수 있습니다</span>
+            </div>
+            <div className={style.main_box}>
+                <label className={style.addPhoto_btn} htmlFor="input" >사진 추가</label>
+                <input onChange={onChangeImage} id="input" type="file" accept="image/jpeg, image/png" className={style.hidden}/>
+                <div className={style.img_box}>
+                    {renderInputImg}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeedAddPhoto;
