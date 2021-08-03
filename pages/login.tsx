5d0a0e414ccf5efe38b15878f39b8de4189d07d4
gudwh14 <alxious@naver.com>
2021-08-03 09:01:01 +0900

    feat : #12 로그인 기능 구현
    리덕스 스토어를 이용하여 로그인 기능 구현 함
    ISSUE#12

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,60 @@
 import style from 'styles/login.module.css'
+import useLogin from "src/store/modules/login/loginHook";
+import {useEffect} from "react";
+import {LoginMember} from "src/apis/Member";
+import {useRouter} from "next/router";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {loginSchema} from "src/utils/yupUtill";
+import Link from "next/link";
 
 const Login = () : JSX.Element => {
+    const {loginMember,error, isLoggedIn} = useLogin();
+    const {register, getValues, handleSubmit, formState : {errors}} = useForm({
+        resolver : yupResolver(loginSchema),
+        mode : "onChange"
+    });
+    const router = useRouter();
+
+
+    const handleLogin = () => {
+        const member : LoginMember = {
+            memberId : getValues('id'),
+            password : getValues('password')
+        }
+        loginMember(member);
+    }
+
+    useEffect(()=> {
+        if(isLoggedIn) {
+            router.push("/");
+        }
+    },[isLoggedIn])
+
+
     return (
         <div className={style.container}>
             <div>
                 <span className={style.logo_txt}>DAILY SHOT</span>
                 <div className={style.login_box}>
-                    <form>
-                        <input className={style.input} placeholder="아이디"/>
-                        <input type="password" className={style.input} placeholder="비밀번호"/>
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <div className={style.input_box}>
+                            <input {...register('id')} className={style.input} placeholder="아이디"/>
+                            <span className={style.input_error_txt}>{errors.id?.message}</span>
+                        </div>
+                        <div className={style.input_box}>
+                            <input {...register('password')} type="password" className={style.input} placeholder="비밀번호"/>
+                            <span className={style.input_error_txt}>{errors.password?.message}</span>
+                        </div>
+                        {error && <span className={style.login_error_txt}>{error.error}</span>}
                         <button className={style.login_btn} type="submit">로그인</button>
                     </form>
                     <div className={style.link_box}>
                         <span className={style.link_txt}>아이디 찾기</span>
                         <span className={style.link_txt}>비밀번호 찾기</span>
-                        <span className={style.link_txt}>회원가입</span>
+                        <Link href={"/signup"}>
+                            <span className={style.link_txt}>회원가입</span>
+                        </Link>
                     </div>
                 </div>
                 <div className={style.divider}></div>
