18b2fac875a36b73ff35c51d8e8858d39631b75a
gudwh14 <alxious@naver.com>
2021-08-08 02:08:05 +0900

    feat : #8 Navbar 피드 페이지용으로 구현
    ISSUE#8

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,17 +1,32 @@
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import style from './navbar.module.css'
+import TagSearch from "../feed/TagSearch";
+import FeedStyleTab from "../feed/FeedStyleTab";
+import {useRouter} from "next/router";
 
 const NavBar = () : JSX.Element => {
+    const router = useRouter();
+
+    const onClickLogin = () => {
+        router.push("/login");
+    }
 
     return (
       <div className={style.container}>
           <div>
-              <div className={style.nav_box}>
-                  <span className={style.logo_txt}>DAILY SHOT</span>
+              <span className={style.logo_txt}>DAILY SHOT</span>
+              <div className={style.feed_box}>
+                  <TagSearch/>
+                  <FeedStyleTab/>
+              </div>
+              <div className={style.user_box}>
                   <div className={style.icon_box}>
                       <AccountCircleOutlinedIcon className={style.icon}/>
                       <FavoriteBorderOutlinedIcon className={style.icon}/>
+                  </div>
+                  <div>
+                      <button className={style.login_btn} onClick={onClickLogin}>로그인</button>
                   </div>
               </div>
           </div>
