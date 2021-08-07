493902700df36663014f8963637e71f51272271e
gudwh14 <alxious@naver.com>
2021-08-08 02:07:16 +0900

    feat : #8 피드 페이지 메인뷰 컴포넌트 UI 구현
    ISSUE#8

---
+++ src/components/feed/main/FeedMain.tsx
@@ -0,0 +1,18 @@
+import style from './feedMain.module.css';
+import FeedList from "./list/FeedList";
+import CardList from "./card/CardList";
+import useFeedType from "src/store/modules/feedType/feedTypeHook";
+import useFeedMenu from "src/store/modules/feedMenu/feedMenuHook";
+
+const FeedMain = () : JSX.Element => {
+    const {type} = useFeedType();
+    const {menu} = useFeedMenu();
+
+    return (
+        <div className={menu === null ? style.container : menu ? style.container_menu_active : style.container_menu_inactive}>
+            {type ? <CardList/> : <FeedList/>}
+        </div>
+    );
+};
+
+export default FeedMain;
