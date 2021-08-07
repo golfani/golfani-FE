aa6d76349a409e1bfac404621e6e65d60f3c4516
gudwh14 <alxious@naver.com>
2021-08-07 17:38:33 +0900

    style : #8 피드 작성페이지 함수 이름 변경
    추후 게시글 작성 페이지 함수 이름과 겹치는 것을 우려해
    Write() -> FeedWrite() 로 이름을 변경함
    ISSUE#8

--- pages/feed/write.tsx
+++ pages/feed/write.tsx
@@ -1,8 +1,8 @@
 import NavBar from "src/components/common/NavBar";
-import FeedAdd from "src/components/feed/FeedAdd";
+import FeedAdd from "src/components/feed/add/FeedAdd";
 import style from "styles/feed.module.css"
 
-const Write = () : JSX.Element => {
+const FeedWrite = () : JSX.Element => {
     return (
         <div className={style.container}>
             <NavBar/>
@@ -11,4 +11,4 @@ const Write = () : JSX.Element => {
     );
 };
 
-export default Write;
+export default FeedWrite;
