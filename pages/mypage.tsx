7a38390ba9a9e5bc857946bf7b150894c4f6d045
gudwh14 <alxious@naver.com>
2021-08-12 14:38:37 +0900

    feat : #17 마이페이지 페이지 구성
    ISSUE#17

---
+++ pages/mypage.tsx
@@ -0,0 +1,20 @@
+import style from 'styles/mypage.module.css'
+import MyPageNavbar from "src/components/mypage/MyPageNavbar";
+import UserProfile from "src/components/mypage/UserProfile";
+import UserActivity from "src/components/mypage/UserActivity";
+import UserFeed from "src/components/mypage/UserFeed";
+
+const MyPage = () : JSX.Element => {
+    return (
+        <div className={style.container}>
+            <MyPageNavbar/>
+            <div className={style.main_container}>
+                <UserProfile/>
+                <UserActivity/>
+                <UserFeed/>
+            </div>
+        </div>
+    );
+};
+
+export default MyPage;
