05d4f08e0c5a5dc6cf6c80eedb339441a5d9f671
gudwh14 <alxious@naver.com>
2021-08-17 11:00:56 +0900

    feat : #19 추천 골프채 판매중인 아이템 컴포넌트 UI 구현
    ISSUE#19

---
+++ src/components/recommend/SellingItem.tsx
@@ -0,0 +1,76 @@
+import style from './sellingItem.module.css';
+import * as faker from "faker";
+
+const SellingItem = () : JSX.Element => {
+    return (
+        <div>
+            <div className={style.item_box}>
+                <img className={style.img} src={faker.image.avatar()}/>
+                <div className={style.main_box}>
+                    <span className={style.category_txt}>아이언</span>
+                    <span className={style.brand_txt}>Mizuno</span>
+                    <span className={style.name_txt}>JPX900 아이언</span>
+                    <span className={style.seller_txt}>Lee_Yong 님이 판매하시는 상품 입니다.</span>
+                </div>
+                <div className={style.price_box}>
+                    <span className={style.price_txt}>169,000 원</span>
+                    <span className={style.buy_txt}>구매하기</span>
+                </div>
+            </div>
+            <div className={style.item_box}>
+                <img className={style.img} src={faker.image.avatar()}/>
+                <div className={style.main_box}>
+                    <span className={style.category_txt}>아이언</span>
+                    <span className={style.brand_txt}>Mizuno</span>
+                    <span className={style.name_txt}>JPX900 아이언</span>
+                    <span className={style.seller_txt}>Lee_Yong 님이 판매하시는 상품 입니다.</span>
+                </div>
+                <div className={style.price_box}>
+                    <span className={style.price_txt}>169,000 원</span>
+                    <span className={style.buy_txt}>구매하기</span>
+                </div>
+            </div>
+            <div className={style.item_box}>
+                <img className={style.img} src={faker.image.avatar()}/>
+                <div className={style.main_box}>
+                    <span className={style.category_txt}>아이언</span>
+                    <span className={style.brand_txt}>Mizuno</span>
+                    <span className={style.name_txt}>JPX900 아이언</span>
+                    <span className={style.seller_txt}>Lee_Yong 님이 판매하시는 상품 입니다.</span>
+                </div>
+                <div className={style.price_box}>
+                    <span className={style.price_txt}>169,000 원</span>
+                    <span className={style.buy_txt}>구매하기</span>
+                </div>
+            </div>
+            <div className={style.item_box}>
+                <img className={style.img} src={faker.image.avatar()}/>
+                <div className={style.main_box}>
+                    <span className={style.category_txt}>아이언</span>
+                    <span className={style.brand_txt}>Mizuno</span>
+                    <span className={style.name_txt}>JPX900 아이언</span>
+                    <span className={style.seller_txt}>Lee_Yong 님이 판매하시는 상품 입니다.</span>
+                </div>
+                <div className={style.price_box}>
+                    <span className={style.price_txt}>169,000 원</span>
+                    <span className={style.buy_txt}>구매하기</span>
+                </div>
+            </div>
+            <div className={style.item_box}>
+                <img className={style.img} src={faker.image.avatar()}/>
+                <div className={style.main_box}>
+                    <span className={style.category_txt}>아이언</span>
+                    <span className={style.brand_txt}>Mizuno</span>
+                    <span className={style.name_txt}>JPX900 아이언</span>
+                    <span className={style.seller_txt}>Lee_Yong 님이 판매하시는 상품 입니다.</span>
+                </div>
+                <div className={style.price_box}>
+                    <span className={style.price_txt}>169,000 원</span>
+                    <span className={style.buy_txt}>구매하기</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SellingItem;
