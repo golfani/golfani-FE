6dec470233fdd8c6850dfaefc8b06c051da5648b
gudwh14 <alxious@naver.com>
2021-08-21 23:40:46 +0900

    feat : #16 태그 입력 기능 구현
    ISSUE#16

--- src/components/feed/add/FeedAddTag.tsx
+++ src/components/feed/add/FeedAddTag.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {FormEvent, useState} from "react";
 import style from "./feedAddTag.module.css";
 
 const FeedAddTag = () : JSX.Element=> {
@@ -6,29 +6,76 @@ const FeedAddTag = () : JSX.Element=> {
     const [size ,setSize] = useState(1);
     const [tagBoxClassName , setTagBoxClassName] = useState(style.tag_input_box_inactive);
     const [tagClassName , setTagClassName] = useState(style.tag_input_inactive);
+    const [tagList, setTagList] = useState<Array<string>>([])
 
-    const change = (event : any) => {
+    const onChange = (event : React.KeyboardEvent<HTMLInputElement>) => {
         const input = event.target as HTMLInputElement;
         const width = input.value.length;
-        console.log(width);
-        console.log("value : ", value.length)
         setSize((size)=>width + 3);
     }
 
-    const leave = () => {
+    const initTagInput = () => {
         setSize(1)
         setValue("");
     }
 
-    const focus = () => {
+    const onBlur = () => {
+        setTagClassName(style.tag_input_inactive);
+        setTagBoxClassName(style.tag_input_box_inactive);
+        initTagInput();
+    }
+
+    const onFocus = () => {
         setTagBoxClassName(style.tag_input_box_active);
         setTagClassName(style.tag_input_active);
     }
+
+    const onAddTagList = () => {
+        if(value.trim().length > 0) {
+            const item = value.replace( /(\s*)/g, "");
+            if(tagList.indexOf(item) === -1) {
+                setTagList(tagList.concat(item)); // space 제거
+            }
+            initTagInput();
+            return;
+        }
+        else return;
+    }
+
+    const handleSubmit = (event : FormEvent) => {
+        event.preventDefault();
+        onAddTagList();
+    }
+
+    const handleClickTag = (id : number) => {
+        onDeleteTag(id);
+    }
+
+    const onDeleteTag = (id : number) => {
+        setTagList((tagList)=> tagList.filter((item,index)=> index !== id));
+    }
+
     return (
         <div className={style.tag_box}>
-            <form className={tagBoxClassName}>
+            {tagList.map((item,index) =>
+                <span
+                    key={item}
+                    className={style.tag_item}
+                    onClick={()=>handleClickTag(index)}>
+                    {item}
+                </span>
+            )}
+            <form className={tagBoxClassName} onSubmit={handleSubmit}>
                 <span>#</span>
-                <input onChange={(e)=>{setValue(e.target.value)}} onFocus={focus} value={value} size={size} onBlur={leave} onKeyDown={change} className={tagClassName} placeholder="태그를 입력해 주세요"/>
+                <input onChange={(e)=>{setValue(e.target.value)}}
+                       onFocus={onFocus}
+                       value={value}
+                       size={size}
+                       onBlur={onBlur}
+                       onKeyDown={onChange}
+                       className={tagClassName}
+                       placeholder="태그를 입력해 주세요"
+                />
                 <button className={style.hidden}></button>
             </form>
         </div>
