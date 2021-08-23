edea27786932113d15f99f7026454b52debaa45e
gudwh14 <alxious@naver.com>
2021-08-23 15:37:46 +0900

    feat : #16 피드작성 페이지 기능 구현
    피드 작성 페이지 기능 구현
    
    ISSUE#16

--- src/components/feed/add/FeedAdd.tsx
+++ src/components/feed/add/FeedAdd.tsx
@@ -1,8 +1,60 @@
 import style from './feedAdd.module.css'
 import FeedAddPhoto from "./FeedAddPhoto";
 import FeedAddContents from "./FeedAddContents";
+import {useState} from "react";
+import {useRouter} from "next/router";
+import Modal from "src/components/modals/Modal";
+import useFeedAdd from "src/store/modules/feedAdd/feedAddHook";
+
+enum FEED_COMPONENT {
+    ADD_PHOTO,
+    ADD_CONTENTS
+}
 
 const FeedAdd = () : JSX.Element => {
+    const {feedAddState} = useFeedAdd();
+    const router = useRouter();
+    const [component, setComponent] = useState<FEED_COMPONENT>(FEED_COMPONENT.ADD_PHOTO);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalMsg, setModalMsg] = useState("");
+
+    const onNext = () => {
+        if(component === FEED_COMPONENT.ADD_PHOTO) {
+            if(feedAddState.imgList.length > 0) {
+                setComponent(FEED_COMPONENT.ADD_CONTENTS);
+            }
+            else {
+                setModalMsg("최소 1장의 사진을 등록해야합니다.");
+                setModalOpen(true);
+            }
+        }
+        else if (component === FEED_COMPONENT.ADD_CONTENTS) {
+            if(feedAddState.content.trim().length <= 0) {
+                setModalMsg("내용을 입력해주세요.")
+                setModalOpen(true)
+            }
+            else {
+                // Todo 피드 작성 API 연동
+            }
+        }
+    }
+
+    const onPrevious = () => {
+        component === FEED_COMPONENT.ADD_PHOTO ? router.back() : setComponent(FEED_COMPONENT.ADD_PHOTO);
+    }
+
+    const onModalSubmit = () => {
+        setModalOpen(false)
+    }
+
+    const handleNextButton = () => {
+        onNext();
+    }
+
+    const handlePreviousButton = () => {
+        onPrevious()
+    }
+
     return (
         <div className={style.container}>
             <div>
@@ -10,13 +62,19 @@ const FeedAdd = () : JSX.Element => {
                 {/*<span className={style.sub_txt}>나만의 피드를 만들어 자랑해 보세요</span>*/}
             </div>
             <div className={style.component_box}>
-                {/*<FeedAddPhoto/>*/}
-                <FeedAddContents/>
+                {component === FEED_COMPONENT.ADD_PHOTO ? <FeedAddPhoto/> : <FeedAddContents/>}
             </div>
             <div className={style.button_box}>
-                <button className={style.prev_btn}>이전</button>
-                <button className={style.next_btn} type="submit">다음</button>
+                <button className={style.prev_btn} onClick={handlePreviousButton}>이전</button>
+                <button className={style.next_btn} onClick={handleNextButton}>
+                    {component === FEED_COMPONENT.ADD_PHOTO ? "다음" : "등록"}
+                </button>
             </div>
+            <Modal
+                open={modalOpen}
+                message= {modalMsg}
+                onSubmit={onModalSubmit}
+            />
         </div>
     );
 };
