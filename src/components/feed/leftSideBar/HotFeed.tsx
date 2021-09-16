13061ae59cf2b4cd13d19c87aebf2ffb02614d6a
gudwh14 <alxious@naver.com>
2021-09-16 11:11:44 +0900

    feat : 인기 피드 더 보러가기 기능 구현

--- src/components/feed/leftSideBar/HotFeed.tsx
+++ src/components/feed/leftSideBar/HotFeed.tsx
@@ -1,27 +1,36 @@
 import style from './hotFeed.module.css';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import FeedThumbnail from "./FeedThumbnail";
-import faker from 'faker';
 import {useInfiniteQuery, useQuery} from "react-query";
-import {getHotFeed, IFeedContent} from "../../../apis/Feed";
-import {IPages} from "../../../domain/Page";
+import {getHotFeed, IFeedContent} from "src/apis/Feed";
+import {IPages} from "src/domain/Page";
+import useFeedType from "src/store/modules/feedType/feedTypeHook";
 
 const HotFeed = () : JSX.Element => {
-    const hotFeedQuery = useQuery<IPages<IFeedContent>>('hotFeed',()=>getHotFeed(),{
-        staleTime : 1000 * 60
+    const {onChangeHotView} = useFeedType()
+    const hotFeedQuery = useInfiniteQuery<IPages<IFeedContent>>('hotFeed',()=>getHotFeed(0,6),{
+        staleTime : 1000 * 60 * 10
     })
+
+    const handleClickMoreTxt = () => {
+        onChangeHotView();
+    }
 
     return (
         <div className={style.container}>
             <div className={style.title_box}>
                 <WhatshotIcon color={"error"} fontSize={"small"}/>
                 <span className={style.title_txt}>인기 피드</span>
-                <span className={style.more_txt}>더 보러가기</span>
+                <span className={style.more_txt} onClick={handleClickMoreTxt}>더 보러가기</span>
             </div>
             <div className={style.thumbnail_box}>
-                {hotFeedQuery.data?.content.map((feed)=> (
-                    <FeedThumbnail feed={feed}/>
-                ))}
+                {
+                    hotFeedQuery.data?.pages.map((page)=>(
+                        page.content.map((feed)=> (
+                            <FeedThumbnail feed={feed}/>
+                        ))
+                    ))
+                }
             </div>
         </div>
     );
--- src/components/feed/main/card/CardList.tsx
+++ src/components/feed/main/card/CardList.tsx
@@ -1,10 +1,12 @@
 import style from './cardList.module.css';
 import CardItem from "./CardItem";
 import {useInfiniteQuery} from "react-query";
-import {getFeed, IFeedContent} from "src/apis/Feed";
+import {getFeed, getHotFeed,IFeedContent} from "src/apis/Feed";
 import {IPages} from "src/domain/Page";
+import useFeedType from "src/store/modules/feedType/feedTypeHook";
 
 const CardList = () : JSX.Element => {
+    const {type} = useFeedType();
     const feedQuery = useInfiniteQuery<IPages<IFeedContent>>('feed',({pageParam = ''})=>getFeed(pageParam,10), {
         getNextPageParam : (lastPage) => {
             const currentPage = lastPage.pageable.pageNumber;
@@ -13,20 +15,45 @@ const CardList = () : JSX.Element => {
             }
             return currentPage + 1;
         },
-        staleTime : 1000 * 60
+        staleTime : 1000 * 60,
+        enabled : type === 'CARD'
     })
 
-    console.log(feedQuery.data);
+    const hotFeedQuery = useInfiniteQuery<IPages<IFeedContent>>('hotFeed',({pageParam = '1'})=>getHotFeed(pageParam,10), {
+        getNextPageParam : (lastPage) => {
+            const currentPage = lastPage.pageable.pageNumber;
+            if(currentPage + 1 >= lastPage.totalPages) {
+                return undefined;
+            }
+            return currentPage + 1;
+        },
+        staleTime : 1000 * 60 * 10,
+        enabled : type === 'HOT'
+    })
+
+    console.log(type);
     return (
         <div className={style.container}>
-            <div className={style.card_box}>
-                {
-                    feedQuery.data?.pages.map((page)=>(
-                        page.content.map((feed)=> (
-                            <CardItem key={feed.id} feed={feed}/>
+            {type === 'HOT' && <span className={style.hotView_txt}>인기 피드게시물</span>}
+            <div className={style.card_container}>
+                <div className={style.card_box}>
+                    {
+                        type === 'CARD' &&
+                        feedQuery.data?.pages.map((page)=>(
+                            page.content.map((feed)=> (
+                                <CardItem key={feed.id} feed={feed}/>
+                            ))
                         ))
-                    ))
-                }
+                    }
+                    {
+                        type === 'HOT' &&
+                        hotFeedQuery.data?.pages.map((page)=>(
+                            page.content.map((feed)=> (
+                                <CardItem key={feed.id} feed={feed}/>
+                            ))
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
