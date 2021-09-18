fbf15ee36a577aa163eb85cfbcd4296dd252d555
gudwh14 <alxious@naver.com>
2021-09-18 17:47:28 +0900

    feat : 내 최근피드 기능 구현

--- src/components/feed/leftSideBar/MyFeed.tsx
+++ src/components/feed/leftSideBar/MyFeed.tsx
@@ -1,27 +1,37 @@
 import style from './myFeed.module.css';
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import FeedThumbnail from "./FeedThumbnail";
-import faker from "faker";
-import {ThumbnailFeed} from "./HotFeed";
+import {useInfiniteQuery} from "react-query";
+import {IPages} from "src/domain/Page";
+import {getUserFeed, IFeedContent} from "src/apis/Feed";
 
-const feeds : ThumbnailFeed[] = [
-    {id : 1, img : faker.image.avatar()},
-    {id : 2, img : faker.image.avatar()},
-    {id : 3, img : faker.image.avatar()},
-    {id : 4, img : faker.image.avatar()},
-    {id : 5, img : faker.image.avatar()},
-    {id : 6, img : faker.image.avatar()},
-];
 
+const userId = "gudwh14"
 const MyFeed = () : JSX.Element => {
+    const myFeedQuery = useInfiniteQuery<IPages<IFeedContent>>(['userFeed',userId],()=>getUserFeed(0,6,userId),{
+        staleTime : 1000 * 60
+    })
+
+    const handleClickMoreTxt = () => {
+        // Todo 마이페이지 -> 내 피드 모두 보러가기 페이지로 이동
+    }
+
     return (
         <div className={style.container}>
             <div className={style.title_box}>
                 <WhatshotIcon color={"error"} fontSize={"small"}/>
                 <span className={style.title_txt}>내 최근 피드</span>
-                <span className={style.more_txt}>더 보러가기</span>
+                <span className={style.more_txt} onClick={handleClickMoreTxt}>더 보러가기</span>
             </div>
-            <FeedThumbnail feeds={feeds}/>
+            <div className={style.thumbnail_box}>
+                {
+                    myFeedQuery.data?.pages.map((page)=>(
+                        page.content.map((feed)=> (
+                            <FeedThumbnail feed={feed}/>
+                        ))
+                    ))
+                }
+            </div>
         </div>
     );
 };
