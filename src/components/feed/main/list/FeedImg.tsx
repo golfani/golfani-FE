a1ced8c7e122109eedd82234ddceb97cdaf006da
gudwh14 <alxious@naver.com>
2021-09-24 18:47:32 +0900

    feat : 피드 이미지 더블킬릭시 좋아요 기능

--- src/components/feed/main/list/FeedImg.tsx
+++ src/components/feed/main/list/FeedImg.tsx
@@ -3,9 +3,12 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {IFeedProps} from "src/domain/Feed";
-import {useQuery} from "react-query";
+import {useMutation, useQuery, useQueryClient} from "react-query";
 import {getFeedPicture, getPictureFile, IPictureDto, PICTURE_API_URL} from "src/apis/Picture";
 import Image from 'next/image';
+import {registerLikes} from "src/apis/Likes";
+import {useCallback, useState} from "react";
+import ToastModal from "src/components/modals/ToastModal";
 
 const CustomNextArrow = ({className, style, onClick} : any) : JSX.Element=> {
     return (
@@ -38,12 +41,42 @@ const settings = {
 };
 
 const FeedImg = ({feed} : IFeedProps) : JSX.Element => {
+    const queryClient = useQueryClient();
     const imageQuery = useQuery<IPictureDto[]>(['feedImg',feed.id],()=>getFeedPicture(feed.id), {
         staleTime : 1000 * 60 * 10
-    })
+    });
+    const [toastModalOpen, setToastModalOpen] = useState(false);
+
+    const likeMutation = useMutation(()=> registerLikes("FEED",feed.id,"gudwh14"));
+
+    const onRegisterLikes = useCallback( async ()=> {
+        const isLike = queryClient.getQueryData(['isFeedLikes',feed.id]);
+        if(!isLike) {
+            try {
+                const response = await likeMutation.mutateAsync();
+            } catch (e) {
+                console.log(e)
+            } finally {
+                await queryClient.invalidateQueries(['feedLikes', feed.id]);
+                await queryClient.invalidateQueries(['isFeedLikes', feed.id]);
+                await onToastMessage();
+            }
+        }
+    },[likeMutation])
+
+    const onToastMessage = () => {
+        setToastModalOpen((toastModalOpen)=> true)
+        setTimeout(()=> {
+            setToastModalOpen((toastModalOpen)=> false);
+        },2300)
+    }
+
+    const handleDoubleClick = async () => {
+        await onRegisterLikes();
+    }
 
     return (
-        <div className={style.container}>
+        <div className={style.container} onDoubleClick={handleDoubleClick}>
             <Slider {...settings}>
                 {imageQuery.data?.map((image)=> (
                     <div key={image.id}>
@@ -51,6 +84,7 @@ const FeedImg = ({feed} : IFeedProps) : JSX.Element => {
                     </div>
                 ))}
             </Slider>
+            {toastModalOpen ? <ToastModal refUserId={feed.userId}/> : <></>}
         </div>
     );
 };
