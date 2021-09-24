ab19498413394825ac562df7ef35ea1bb678eb93
gudwh14 <alxious@naver.com>
2021-09-24 19:27:41 +0900

    feat : 피드 글쓰기 버튼 생성

--- src/components/feed/main/FeedMain.tsx
+++ src/components/feed/main/FeedMain.tsx
@@ -3,14 +3,29 @@ import FeedList from "./list/FeedList";
 import CardList from "./card/CardList";
 import useFeedType from "src/store/modules/feedType/feedTypeHook";
 import useFeedMenu from "src/store/modules/feedMenu/feedMenuHook";
+import AddIcon from '@material-ui/icons/Add';
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const FeedMain = () : JSX.Element => {
     const {type} = useFeedType();
-    const {menu} = useFeedMenu();
+    const {menu,onInitMenu} = useFeedMenu();
+    const router = useRouter();
+
+    useEffect(()=> {
+        onInitMenu();
+    },[])
+
+    const handleClickWrite = () => {
+        router.push('feed/write');
+    }
 
     return (
         <div className={menu === null ? style.container : menu ? style.container_menu_active : style.container_menu_inactive}>
             {type === 'LIST'? <FeedList/> : <CardList/>}
+            <div className={style.write_box} onClick={handleClickWrite}>
+                <AddIcon className={style.write_icon}/>
+            </div>
         </div>
     );
 };
