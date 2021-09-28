adc108ce7f8153041d00b507d0281cffd8ad552f
gudwh14 <alxious@naver.com>
2021-09-28 13:48:06 +0900

    feat : 로그인 유무 체크 기능 추가

--- src/components/feed/FeedNavBar.tsx
+++ src/components/feed/FeedNavBar.tsx
@@ -6,9 +6,11 @@ import FeedStyleTab from "./FeedStyleTab";
 import {useRouter} from "next/router";
 import MyMenu from "../common/MyMenu";
 import {useRef, useState} from "react";
-import {handleClickRefOutSide} from "../../utils/clickUtil";
+import {handleClickRefOutSide} from "src/utils/clickUtil";
+import useLogin from "src/store/modules/login/loginHook";
 
 const FeedNavBar = () : JSX.Element => {
+    const login = useLogin();
     const [myMenuOpen, setMyMenuOpen] = useState(false);
     const targetRef = useRef<HTMLDivElement>(null);
     const router = useRouter();
@@ -36,16 +38,20 @@ const FeedNavBar = () : JSX.Element => {
                   <FeedStyleTab/>
               </div>
               <div className={style.user_box}>
-                  <div className={style.icon_box}>
-                      <div className={style.myMenu_box} ref={targetRef}>
-                        <AccountCircleOutlinedIcon className={style.icon} onClick={handleClickMyMenu}/>
-                        <MyMenu open={myMenuOpen}/>
+                  {login.isLoggedIn
+                      ?
+                      <div className={style.icon_box}>
+                          <div className={style.myMenu_box} ref={targetRef}>
+                              <AccountCircleOutlinedIcon className={style.icon} onClick={handleClickMyMenu}/>
+                              <MyMenu open={myMenuOpen}/>
+                          </div>
+                          <div>
+                            <FavoriteBorderOutlinedIcon className={style.icon}/>
+                          </div>
                       </div>
-                      <FavoriteBorderOutlinedIcon className={style.icon}/>
-                  </div>
-                  <div>
-                      <button className={style.login_btn} onClick={onClickLogin}>로그인</button>
-                  </div>
+                      :
+                          <span className={style.login_btn} onClick={onClickLogin}>로그인</span>
+                  }
               </div>
           </div>
       </div>
