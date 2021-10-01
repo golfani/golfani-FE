79fff981e8ed49e7a8d9f06c8b410a8765a363a5
gudwh14 <alxious@naver.com>
2021-10-01 15:38:00 +0900

    feat : 아이디 찾기 페이지 생성

---
+++ pages/member/findId.tsx
@@ -0,0 +1,116 @@
+import style from 'styles/find.module.css';
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {emailSchema} from "src/utils/yupUtil";
+import {useEffect, useState} from "react";
+import {authMailForFindUser, fetchAuthCode, findUserIdByEmail} from "src/apis/Member";
+import Link from 'next/link';
+import useLogin from "src/store/modules/login/loginHook";
+import {useRouter} from "next/router";
+
+interface IForm {
+    email : string
+}
+
+const FindId = () : JSX.Element => {
+    const {register,getValues, handleSubmit, formState : {errors}} = useForm<IForm>({
+        resolver : yupResolver(emailSchema),
+        mode : 'onChange'
+    });
+    const [bindInput, setBindInput] = useState(false);
+    const [userId, setUserId] = useState('');
+    const [authCode, setAuthCode] = useState('');
+    const [authCodeError, setAuthCodeError] = useState('');
+    const router = useRouter();
+    const login = useLogin();
+
+    const onSendEmail = async (data : IForm) => {
+        try {
+            setAuthCodeError('');
+            const response = await authMailForFindUser(data.email);
+            if(response.status === 200) {
+                setBindInput(true);
+            }
+        }
+        catch (e) {
+            if(e.response.status === 409) {
+                alert('존재하지 않는 이메일 입니다.');
+            }
+            else if(e.response.status === 500) {
+                alert('잠시 후 다시 시도해 주세요.');
+            }
+        }
+    }
+
+    const onFindUserId = async () => {
+        const email = getValues('email');
+        try {
+            const authResponse = await fetchAuthCode(email,authCode);
+            if(authResponse) {
+                const findResponse = await findUserIdByEmail(email);
+                setUserId(findResponse.data);
+            }
+            else {
+                setAuthCodeError('인증번호가 일치하지 않습니다');
+            }
+        }
+        catch (e) {
+            alert('잠시 후 다시 시도해 주세요.');
+        }
+    }
+
+    useEffect(()=> {
+        if(login.isLoggedIn) {
+            router.push('/');
+        }
+    },[])
+
+    return (
+        <div className={style.container}>
+            <div className={style.box}>
+                <div className={style.find_box}>
+                    <span className={style.logo_txt}>GOLFANI</span>
+                    <div className={style.title_box}>
+                        <Link href={'/member/findId'}>
+                            <span className={style.title_txt_active}>아이디 찾기</span>
+                        </Link>
+                        <Link href={'/member/findPw'}>
+                            <span className={style.title_txt}>비밀번호 찾기</span>
+                        </Link>
+                    </div>
+                    <form className={style.input_box} onSubmit={handleSubmit(onSendEmail)}>
+                        <input className={style.input} type={'text'} placeholder={'이메일 입력'} {...register('email')}/>
+                        <button className={style.mail_btn} disabled={!getValues('email')}>인증번호 전송</button>
+                    </form>
+                    <div>
+                        <span className={style.input_error_txt}>{errors.email?.message}</span>
+                    </div>
+                    {bindInput &&
+                    <div className={style.input_box}>
+                        <input className={style.input} type={'text'} placeholder={'인증번호'} value={authCode}
+                               onChange={(e) => setAuthCode(e.target.value)}/>
+                        <button className={style.mail_btn} disabled={!authCode} onClick={onFindUserId}>인증</button>
+                    </div>
+                    }
+                    <div>
+                        <span className={style.input_error_txt}>{authCodeError}</span>
+                    </div>
+                    {userId &&
+                    <div>
+                        <div className={style.divider}></div>
+                        <div className={style.result_box}>
+                            <span className={style.userId_info}>회원 ID</span>
+                            <span className={style.userId_txt}>{userId}</span>
+                        </div>
+                        <Link href={'/login'}>
+                            <span className={style.login_link_txt}>로그인 하러가기</span>
+                        </Link>
+                    </div>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FindId;
