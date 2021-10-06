62dd05cffbb734626b826cd2c3d1e4316adca6a2
gudwh14 <alxious@naver.com>
2021-10-06 17:46:31 +0900

    feat : axios -> securityAxios 에 header 추가 변경

--- src/store/modules/login/saga.ts
+++ src/store/modules/login/saga.ts
@@ -2,20 +2,22 @@ import {PayloadAction} from "@reduxjs/toolkit";
 import {login, LoginMember} from "src/apis/Member";
 import {call, put, takeEvery} from "@redux-saga/core/effects";
 import {loginAsync, loginAsyncSuccess, loginAsyncError, IUser} from './login';
-import axios from "axios";
 import {setCookie} from "src/utils/cookieUtil";
+import {securityAxios} from "src/security/axios";
 
 function* handleLoginSaga(action : PayloadAction<LoginMember>) {
     try {
         const user : IUser = yield call(login, action.payload);
-        yield axios.defaults.headers.common['Authorization'] = `Bearer ${user.accessToken}`;
+        yield securityAxios.defaults.headers.common['Authorization'] = `Bearer ${user.accessToken}`;
         yield setCookie('refreshToken',user.refreshToken, {
             path : '/',
             secure : false,
+            maxAge : 60 * 60 * 24 * 7,
         });
         yield setCookie('userId',user.userId, {
             path : '/',
             secure : false,
+            maxAge : 60 * 60 * 24 * 7,
         });
         yield put(loginAsyncSuccess());
     }
