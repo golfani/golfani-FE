6cf6258f5803b203632da0cbda738da46c08ef2d
gudwh14 <alxious@naver.com>
2021-10-08 17:15:06 +0900

    feat : AccessToken silent-refresh 기능 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,9 @@ import {ReactQueryDevtoolsPanel} from "react-query/devtools";
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from "redux-persist";
 import {Provider} from "react-redux";
+import {useEffect} from "react";
+import {getCookie} from "src/utils/cookieUtil";
+import {onSilentRefresh} from "src/utils/securityUtil";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,6 +17,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   const reduxStore = store();
   const persistor = persistStore(reduxStore);
+
+  // 로그인 상태일시 silentRefresh 진행
+  useEffect(()=> {
+      const userId = getCookie('userId');
+      userId && onSilentRefresh(userId);
+  },[]);
 
   return (
       <QueryClientProvider client={queryClient}>
---
+++ src/utils/securityUtil.ts
@@ -0,0 +1,24 @@
+import {regenerateAccessToken} from "../apis/Member";
+import {securityAxios} from "../security/axios";
+import {removeCookie} from "./cookieUtil";
+
+const onRegenerateAccessToken = async (userId : string) => {
+    try {
+        const response = await regenerateAccessToken(userId);
+        securityAxios.defaults.headers.common['Authorization'] = `Bearer ${response.data}`;
+    }
+    catch (e) {
+        removeCookie('userId');
+        removeCookie('refreshToken');
+        window.location.href = 'login';
+        alert('로그인 세션 만료, 다시 로그인 해주세요');
+    }
+}
+
+export const onSilentRefresh = async (userId : string) => {
+    const ACCESS_TOKEN_EXPIRE = 1000 * 60 * 30; // ms
+    await onRegenerateAccessToken(userId);
+    setTimeout(async ()=> {
+        await onSilentRefresh(userId);
+    },ACCESS_TOKEN_EXPIRE - 6000 * 10); // ms
+}
