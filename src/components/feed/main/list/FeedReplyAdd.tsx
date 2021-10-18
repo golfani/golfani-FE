ac4415cf23fcefa662146b8b64e94ba2c52b1812
gudwh14 <alxious@naver.com>
2021-10-18 16:14:41 +0900

    feat : 피드 댓글 프로필 이미지 보여주기

--- src/components/feed/main/list/FeedReplyAdd.tsx
+++ src/components/feed/main/list/FeedReplyAdd.tsx
@@ -1,12 +1,17 @@
 import style from './feedReplyAdd.module.css';
-import GitHubIcon from '@material-ui/icons/GitHub';
 import {IFeedReplyAddProps} from "src/domain/Reply";
+import Image from 'next/image';
 import FeedReplyAddInput from "./FeedReplyAddInput";
+import {getCookie} from "src/utils/cookieUtil";
+import {getProfileImage} from "src/apis/Member";
 
 const FeedReplyAdd =({feedId} : IFeedReplyAddProps) => {
+    const userId = getCookie('userId');
     return(
         <div className={style.container}>
-            <GitHubIcon className={style.icon}/>
+            <div className={style.img_box}>
+                <Image src={getProfileImage(userId,'MID')} className={style.img} width={35} height={35} quality={100}/>
+            </div>
             <FeedReplyAddInput feedId={feedId} refId={null} refUser={null}/>
         </div>
     );
