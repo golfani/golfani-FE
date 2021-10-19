c906701621df3cf191f08d8b212b7afaee9d3d2a
gudwh14 <alxious@naver.com>
2021-10-19 13:17:54 +0900

    feat : #195 NavbarLink 클릭 범위 수정

--- src/components/common/navbar/NavbarLink.tsx
+++ src/components/common/navbar/NavbarLink.tsx
@@ -6,21 +6,25 @@ const NavbarLink = () : JSX.Element => {
     const router = useRouter();
     const where = router.pathname.split('/')[1];
 
+    const onRoutePage = (page : string) => {
+        router.push(`/${page}`);
+    }
+
+    const handleClickLinkButton = (page : string) => {
+        onRoutePage(page);
+    }
+
     return (
         <div className={style.logo_box}>
             <Link href={'/'}>
                 <span className={style.logo_txt}>GOLF ANI</span>
             </Link>
             <ul className={style.link_box}>
-                <li className={where === 'feed' ? style.list_item_active : style.list_item}>
-                    <Link href={'/feed'}>
-                        <span>피드</span>
-                    </Link>
+                <li className={where === 'feed' ? style.list_item_active : style.list_item} onClick={()=>handleClickLinkButton('feed')}>
+                    <span>피드</span>
                 </li>
-                <li className={where === 'board' ? style.list_item_active : style.list_item}>
-                    <Link href={'/board'}>
-                        <span>커뮤니티</span>
-                    </Link>
+                <li className={where === 'board' ? style.list_item_active : style.list_item} onClick={()=>handleClickLinkButton('board')}>
+                    <span>커뮤니티</span>
                 </li>
             </ul>
         </div>
