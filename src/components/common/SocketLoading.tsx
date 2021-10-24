8f5563db3630e2b3c2c4cdf49288e9521d073f31
gudwh14 <alxious@naver.com>
2021-10-24 11:40:35 +0900

    feat : issue#220 SocketLoading 컴포넌트 생성

---
+++ src/components/common/SocketLoading.tsx
@@ -0,0 +1,11 @@
+
+const SocketLoading = () => {
+    return (
+        <div style={{display : "flex", flexDirection : "column", justifyContent : "center", alignItems : "center", height : '100vh', opacity : '0.5'}}>
+            <span style={{fontSize : 50, fontWeight : 500}}>GOLF ANI</span>
+            <span style={{fontSize : 20, fontWeight : 500}}>로딩중</span>
+        </div>
+    );
+};
+
+export default SocketLoading;
