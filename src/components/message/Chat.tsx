541829da10b52d2413768a1cf4f6476ad09cba7b
gudwh14 <alxious@naver.com>
2021-11-03 23:46:26 +0900

    feat : issue#271 채팅 메세지 리스트 scroll문제 해결(css)

--- src/components/message/Chat.tsx
+++ src/components/message/Chat.tsx
@@ -11,17 +11,39 @@ import ChatItem from "./ChatItem";
 const Chat = () : JSX.Element => {
     const chatScrollRef = useRef<HTMLDivElement>(null);
     const chatRoom = useChatRoom();
-    const chatMessageQuery = useInfiniteQuery<IPages<IChatMessageDto>>(['chatMessage',chatRoom.activeChatRoom?.id],
-        ()=>getChatMessageByRoomId(chatRoom.activeChatRoom?.id!,0,20), {
-            enabled : chatRoom.activeChatRoom?.id !== undefined,
-            staleTime : 6000 * 10
+    const chatMessageQuery = useInfiniteQuery<IPages<IChatMessageDto>>(['chatMessage', chatRoom.activeChatRoom?.id],
+        ({pageParam = 0}, roomId = chatRoom.activeChatRoom?.id!) => getChatMessageByRoomId(roomId, pageParam, 20), {
+            getNextPageParam: (lastPage) => {
+                const currentPage = lastPage.pageable.pageNumber;
+                if(currentPage + 1 >= lastPage.totalPages) {
+                    return undefined;
+                }
+                return currentPage + 1;
+            },
+            enabled: chatRoom.activeChatRoom?.id !== undefined,
+            staleTime: 6000 * 10
         })
     const queryClient = useQueryClient();
     const [isSendBySelf, setIsSendBySelf] = useState(false);
+    const chatTopScrollRef = useRef<HTMLDivElement>(null);
+    const observer = useRef<IntersectionObserver>();
 
     const chatScrollDown = () => {
         chatScrollRef.current?.scrollIntoView({ block: 'end', inline: 'nearest'});
     }
+
+    const intersectionObserver = (entries : IntersectionObserverEntry[], io : IntersectionObserver) => {
+        entries.forEach(async (entry) => {
+            if(entry.isIntersecting) {
+                await chatMessageQuery.fetchNextPage();
+            }
+        })
+    }
+
+    useEffect(()=> {
+        observer.current = new IntersectionObserver(intersectionObserver);
+        chatTopScrollRef.current && observer.current?.observe(chatTopScrollRef.current);
+    },[chatRoom.activeChatRoom])
 
     useEffect(()=> {
         isSendBySelf && chatScrollDown();
@@ -34,23 +56,32 @@ const Chat = () : JSX.Element => {
 
     return (
         <div className={style.container}>
-            <div className={style.chat_title_box}>
-                {chatRoom.activeChatRoom &&
-                <img src={getProfileImage(chatRoom.activeChatRoom?.receiver,'MID')} className={style.title_img}/>
-                }
-                <span className={style.title_userId_txt}>{chatRoom.activeChatRoom?.receiver}</span>
-            </div>
-            <div className={style.chat_box}>
-                {
-                    chatMessageQuery.data?.pages.map((page) => (
-                        page.content.map((chat,index) => (
-                            <ChatItem key={chat.id} chat={chat} chatData={page.content} index={index}/>
-                        ))
-                    ))
-                }
-                <div ref={chatScrollRef}></div>
-            </div>
-            <ChatInput setIsSendBySelf={setIsSendBySelf}/>
+            {chatRoom.activeChatRoom ?
+                <div>
+                    <div className={style.chat_title_box}>
+                        {chatRoom.activeChatRoom &&
+                        <img src={getProfileImage(chatRoom.activeChatRoom?.receiver,'MID')} className={style.title_img}/>
+                        }
+                        <span className={style.title_userId_txt}>{chatRoom.activeChatRoom?.receiver}</span>
+                    </div>
+                    <div className={style.chat_box}>
+                        <div ref={chatScrollRef}> </div>
+                        {
+                            chatMessageQuery.data?.pages.map((page, index) => {
+                                return (
+                                    <div key={index}>
+                                        {page.content.map((chat, index) => (
+                                            <ChatItem key={chat.id} chat={chat} chatData={page.content} index={index}/>
+                                        ))}
+                                    </div>
+                                )
+                            })
+                        }
+                        <div ref={chatTopScrollRef}> </div>
+                    </div>
+                    <ChatInput setIsSendBySelf={setIsSendBySelf}/>
+                </div>
+                : <p className={style.show_txt}>사용자들과 채팅을 나눠보세요!</p>}
         </div>
     );
 };
