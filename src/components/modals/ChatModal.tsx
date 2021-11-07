40eced0b83a405d8a62691b56f7449cd3e7fad7e
gudwh14 <alxious@naver.com>
2021-11-08 00:21:54 +0900

    feat : issue#300 채팅 모달에서 바디 스크롤 막기

--- src/components/modals/ChatModal.tsx
+++ src/components/modals/ChatModal.tsx
@@ -1,11 +1,15 @@
 import style from './chatModal.module.css';
 import Chat from "../message/Chat";
+import {bodyScrollActionForModal} from "src/utils/scrollUtil";
 
 interface IChatModalProps {
     closeModal : () => void
 }
 
 const ChatModal = (props : IChatModalProps) : JSX.Element => {
+
+    bodyScrollActionForModal();
+
     return (
         <div className={style.modal}>
             <div className={style.chat_box}>
