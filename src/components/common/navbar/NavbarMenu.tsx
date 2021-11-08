0383d400ad42c92d233ebe2b912fe428f7b06bf0
gudwh14 <alxious@naver.com>
2021-11-08 15:02:37 +0900

    feat : issue#301 Navbar css 변경

--- src/components/common/navbar/NavbarMenu.tsx
+++ src/components/common/navbar/NavbarMenu.tsx
@@ -1,64 +1,64 @@
 import style from "./navbarMenu.module.css";
-import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
-import MyMenu from "../MyMenu";
 import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
 import Alarm from "src/components/common/alarm/Alarm";
 import {useRef, useState} from "react";
-import {useRouter} from "next/router";
 import {handleClickRefOutSide} from "src/utils/clickUtil";
 import {getCookie} from "src/utils/cookieUtil";
 import {useQuery} from "react-query";
 import {getUnreadAlarmCount} from "src/apis/Alarm";
-import {getUnreadChatMessageCount} from "../../../apis/Chat";
+import {getUnreadChatMessageCount} from "src/apis/Chat";
+import useCustomRouter from "src/hooks/routerHook";
+import {getProfileImage} from "src/apis/Member";
 
 const NavbarMenu = () => {
     const userId = getCookie('userId')
-    const [myMenuOpen, setMyMenuOpen] = useState(false);
     const [noticeOpen, setNoticeOpen] = useState(false);
-    const myOpenRef = useRef<HTMLDivElement>(null);
     const noticeRef = useRef<HTMLDivElement>(null);
-    const router = useRouter();
+    const {onConflictRoute} = useCustomRouter();
     const unReadAlarmQuery = useQuery('unReadAlarm', ()=>getUnreadAlarmCount(),{
         staleTime : 60 * 10 * 1000
     });
     const unReadMessageQuery = useQuery('unReadMessage', () => getUnreadChatMessageCount());
 
-    const onCloseMyMenu = () => {
-        setMyMenuOpen((myMenuOpen)=> false);
-    }
-
     const onCloseNotice = () => {
         setNoticeOpen(false);
     }
 
-    const handleClickMyMenu = () => {
-        setMyMenuOpen((myMenuOpen)=> !myMenuOpen);
+    const handleClickProfile = () => {
+        onConflictRoute(`/profile/${userId}`);
     }
 
     const handleClickNotice = () => {
         setNoticeOpen((noticeOpen)=> !noticeOpen);
     }
 
-    handleClickRefOutSide(myOpenRef,onCloseMyMenu);
+    const handleClickChat = () => {
+        onConflictRoute(`/message`);
+    }
+
     handleClickRefOutSide(noticeRef,onCloseNotice);
 
     const onClickLogin = () => {
-        router.push("/login");
+        onConflictRoute(`/login`);
     }
+
     return (
         <div className={style.user_box}>
             {userId
                 ?
                 <div className={style.icon_box}>
-                    <div className={style.menu_box} ref={myOpenRef}>
-                        <AccountCircleOutlinedIcon className={style.icon} onClick={handleClickMyMenu} fontSize={'inherit'}/>
-                        {unReadMessageQuery.data !== 0 && <span className={style.notice_count}>{unReadMessageQuery.data}</span>}
-                        {myMenuOpen && <MyMenu open={myMenuOpen}/>}
-                    </div>
                     <div className={style.menu_box} ref={noticeRef}>
                         <FavoriteBorderOutlinedIcon className={style.icon} onClick={handleClickNotice} fontSize={'inherit'}/>
                         {unReadAlarmQuery.data !== 0 && <span className={style.notice_count}>{unReadAlarmQuery.data}</span>}
                         {noticeOpen && <Alarm/>}
+                    </div>
+                    <div className={style.menu_box}>
+                        <ChatBubbleOutlineRoundedIcon className={style.icon} onClick={handleClickChat} fontSize={'inherit'}/>
+                        {unReadMessageQuery.data !== 0 && <span className={style.notice_count}>{unReadMessageQuery.data}</span>}
+                    </div>
+                    <div className={style.menu_box} onClick={handleClickProfile}>
+                        <img className={style.img} src={getProfileImage(userId,'MID')} width={26} height={26}/>
                     </div>
                 </div>
                 :
