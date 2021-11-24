0fafcd3d1e0bbd46d40f117b48d272958ff88e76
LYJ <rbdnjs251@naver.com>
2021-11-24 22:53:48 +0900

    feat : issue#384

--- src/components/board/main/view/BoardView.tsx
+++ src/components/board/main/view/BoardView.tsx
@@ -61,9 +61,10 @@ const BoardView = ({boardView} : IBoardProps): JSX.Element => {
         img.onload = () => {
             const popupX = (window.screen.width / 2) - (img.width / 2);
             const popupY = (window.screen.height / 2) - (img.height / 2);
-            window.open(img.src,'',`height= ${img.height}, width=${img.width}, left=${popupX}, top=${popupY}, location=no,status=no,scrollbars=yes`);
+            window.open(img.src, '', `height= ${img.height}, width=${img.width}, left=${popupX}, top=${popupY}, location=no,status=no,scrollbars=yes`);
         }
         img.src = url;
+    }
 
     const onUserIdClick = () =>{
         router.push(`/profile/${boardView.userId}`);
