a4fe971d3fd15a1728b28b45f167bcde1e990370
gudwh14 <alxious@naver.com>
2021-12-12 22:35:56 +0900

    feat : issue#460 샵 페이지 스토어 등록 버튼 추가

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -3,8 +3,16 @@ import Navbar from "src/components/common/navbar/Navbar";
 import ShopSearch from "src/components/shop/ShopSearch";
 import ShopHotStoreList from "src/components/shop/ShopHotStoreList";
 import ShopRecommendGolfClub from "src/components/shop/ShopRecommendGolfClub";
+import {useState} from "react";
+import ShopRegisterModal from "src/components/modals/ShopRegisterModal";
 
 const Shop = () : JSX.Element => {
+    const [openShopRegisterModal, setOpenShopRegisterModal] = useState(false);
+
+    const handleClickShopAddButton = () => {
+        setOpenShopRegisterModal(true);
+    }
+
     return (
         <div className={style.container}>
             <Navbar/>
@@ -13,6 +21,14 @@ const Shop = () : JSX.Element => {
                 <ShopSearch/>
                 <ShopHotStoreList/>
                 <ShopRecommendGolfClub/>
+                {openShopRegisterModal && <ShopRegisterModal setModalOpen={setOpenShopRegisterModal}/>}
+            </div>
+            <div className={style.shop_add_box} onClick={handleClickShopAddButton}>
+                <img src={'/icon/shop_ico.png'} alt={'shop_add'} className={style.shop_img}/>
+                <div>
+                    <span className={style.shop_add_txt}>스토어 등록</span>
+                    <span className={style.shop_add_sub_txt}>운영중인 매장을 등록해보세요</span>
+                </div>
             </div>
         </div>
     );
