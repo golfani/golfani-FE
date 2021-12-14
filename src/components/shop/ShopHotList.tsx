b1344e07015495ac9960627e85c688b5e5f0ec2f
gudwh14 <alxious@naver.com>
2021-12-14 22:57:06 +0900

    feat : issue#472 스토어 인기 아이템 컴포넌트 생성

---
+++ src/components/shop/ShopHotList.tsx
@@ -0,0 +1,18 @@
+import style from './shopHotList.module.css';
+import ShopItem from "./ShopItem";
+
+const ShopHotList = () : JSX.Element => {
+    return (
+        <div className={style.container}>
+            <span className={style.title_txt}>인기 상품</span>
+            <div className={style.item_box}>
+                <ShopItem/>
+                <ShopItem/>
+                <ShopItem/>
+                <ShopItem/>
+            </div>
+        </div>
+    );
+};
+
+export default ShopHotList;
