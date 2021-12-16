380caaf5c25ea56a4855b577df5e4432f6b62df2
LYJ <rbdnjs251@naver.com>
2021-12-16 21:40:01 +0900

    issue test

--- pages/board/write.tsx
+++ pages/board/write.tsx
@@ -1,7 +1,6 @@
 import Navbar from "../../src/components/common/navbar/Navbar";
 import BoardWrite from 'src/components/board/BoardWrite';
 import BoardListHead from "../../src/components/board/item/BoardListHead";
-import {EBoardType} from "../../src/domain/board";
 
 const write = (): JSX.Element => {
     return(
