4eac48e7731b32132ae520d2549e738f4707f63e
gudwh14 <alxious@naver.com>
2021-12-27 12:16:23 +0900

    style : manage page code formatting & move dir

---
+++ pages/shop/manage/[shop].tsx
@@ -0,0 +1,15 @@
+import Navbar from "src/components/common/navbar/Navbar";
+import ShopInfo from "src/components/shop/manage/ShopInfo";
+import ShopManageMenu from "src/components/shop/manage/ShopManageMenu";
+
+const ShopManagePage = () : JSX.Element =>{
+    return(
+        <div>
+            <Navbar/>
+            <ShopInfo/>
+            <ShopManageMenu/>
+        </div>
+    )
+}
+
+export default ShopManagePage;
