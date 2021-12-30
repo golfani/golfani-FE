dce28e458aa2d0c9f1731083c50d3ff2753959f2
gudwh14 <alxious@naver.com>
2021-12-30 21:43:11 +0900

    fix : issue#552 카테고리 선택후 nav 에서 커뮤니티 클릭시 홈으로 이동안되는 현상 수정

--- src/components/board/BoardCategory.tsx
+++ src/components/board/BoardCategory.tsx
@@ -1,43 +1,27 @@
 import style from 'src/components/board/boardCategory.module.css';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {EBoardType} from "src/domain/board";
-import {useRouter} from "next/router";
 import HomeIcon from '@material-ui/icons/Home';
 import BoardSearchBar from "./search/BoardSearchBar";
 import SearchIcon from '@material-ui/icons/Search';
+import useCustomRouter from "src/hooks/routerHook";
 
 interface IBoardTypeProps {
     onSetBoardType : (type:EBoardType) => void
+    boardType : EBoardType
 }
 
-const BoardCategory = (props: IBoardTypeProps): JSX.Element => {
-    const router = useRouter();
-    const {type} = router.query;
-    const [selectMenu, setMenu] = useState<EBoardType>(type as EBoardType);
+const BoardCategory = ({onSetBoardType, boardType}: IBoardTypeProps): JSX.Element => {
+    const customRouter = useCustomRouter();
     const [openSearch, setOpenSearch] = useState(false);
 
-    useEffect(()=>{
-        if(type === undefined) {
-            props.onSetBoardType(EBoardType.HOME);
-            setMenu(EBoardType.HOME);
-        }
-    },[])
-
-    useEffect(()=>{
-        if(type !== undefined){
-            props.onSetBoardType(type as EBoardType);
-            setMenu(type as EBoardType);
-        }
-    },[type]);
-
     const menuClicked = (type : EBoardType) =>{
-        if(type !== selectMenu) setMenu(type);
         if(type === EBoardType.HOME) {
-            router.push('/board');
+            customRouter.onConflictRoute('/board');
         } else {
-            router.push(`/board?type=${type}&page=0`);
+            customRouter.onConflictRoute(`/board?type=${type}&page=0`);
         }
-        props.onSetBoardType(type);
+        onSetBoardType(type);
     }
 
     const handleClickSearchIcon = () => {
@@ -47,13 +31,13 @@ const BoardCategory = (props: IBoardTypeProps): JSX.Element => {
     return(
         <div className={style.container}>
             <ul className={style.menu_category}>
-                <HomeIcon className={selectMenu === EBoardType.HOME ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.HOME)}}/>
-                <li className={selectMenu === EBoardType.FREE ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.FREE)}}>자유게시판</li>
-                <li className={selectMenu === EBoardType.ANONYMOUS ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.ANONYMOUS)}}>익명게시판</li>
-                <li className={selectMenu === EBoardType.TIP ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.TIP)}}>정보게시판</li>
-                <li className={selectMenu === EBoardType.REVIEW ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.REVIEW)}}>후기게시판</li>
-                <li className={selectMenu === EBoardType.TRADE ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.TRADE)}}>거래게시판</li>
-                <li className={selectMenu === EBoardType.ASK ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.ASK)}}>문의게시판</li>
+                <HomeIcon className={boardType === EBoardType.HOME ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.HOME)}}/>
+                <li className={boardType === EBoardType.FREE ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.FREE)}}>자유게시판</li>
+                <li className={boardType === EBoardType.ANONYMOUS ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.ANONYMOUS)}}>익명게시판</li>
+                <li className={boardType === EBoardType.TIP ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.TIP)}}>정보게시판</li>
+                <li className={boardType === EBoardType.REVIEW ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.REVIEW)}}>후기게시판</li>
+                <li className={boardType === EBoardType.TRADE ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.TRADE)}}>거래게시판</li>
+                <li className={boardType === EBoardType.ASK ? style.board_menu_clicked : style.board_menu_default} onClick={() => {menuClicked(EBoardType.ASK)}}>문의게시판</li>
             </ul>
             <div className={style.search_box}>
                 {openSearch
