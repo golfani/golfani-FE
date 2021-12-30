5feb10121fa1ca433e817a7886fcfc591e6659a7
gudwh14 <alxious@naver.com>
2021-12-30 16:20:22 +0900

    fix : issue#532 경로에 한글이 포함 될경우 버그 발생 수정

--- src/hooks/routerHook.ts
+++ src/hooks/routerHook.ts
@@ -4,7 +4,11 @@ const useCustomRouter = () => {
     const router = useRouter();
 
     const onConflictRoute = (path : string) => {
-        if(router.asPath === encodeURI(path)) {
+        /*
+            path 에 한글이 포함된경우 asPath, encodeURI 가 다르게 보여짐
+            router.asPath encode 하여 비교해야함
+         */
+        if(encodeURI(router.asPath) === encodeURI(path)) {
             if(typeof window !== 'undefined') {
                 window.location.href = path;
             }
