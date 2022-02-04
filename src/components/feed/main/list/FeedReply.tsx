0120f624140ba55a44021bc7a503e9815d22abc6
gudwh14 <alxious@naver.com>
2022-02-04 10:04:01 +0900

    fix : issue#617 피드 댓글 더보기 버그 수정

--- src/components/feed/main/list/FeedReply.tsx
+++ src/components/feed/main/list/FeedReply.tsx
@@ -6,35 +6,48 @@ import {IPages} from "src/domain/Page";
 import {getFeedReply, IReplyDto} from "src/apis/Reply";
 import {IFeedItemProps} from "./FeedItem";
 
-const FeedReply = ({feed, isModal} : IFeedItemProps) => {
-    const replyQuery = useInfiniteQuery<IPages<IReplyDto>,Error>(['feedReply',feed.id],({pageParam = '3'}) =>  getFeedReply(feed.id,0,pageParam), {
-        getNextPageParam : (lastPage ) => {
+const FeedReply = ({feed, isModal}: IFeedItemProps) => {
+    const replyQuery = useInfiniteQuery<IPages<IReplyDto>, Error>(['feedReply', feed.id], ({pageParam = 3}) => getFeedReply(feed.id, 0, pageParam), {
+        getNextPageParam: (lastPage) => {
             const currentPage = lastPage.pageable.pageNumber;
-            if(currentPage === 0 && !lastPage.last) {
+            if (currentPage === 0 && !lastPage.last) {
                 return lastPage.totalElements;
             }
             return undefined;
         },
-        staleTime : 1000 * 60
+        staleTime: 1000 * 60 * 10
     });
 
     const handleClickMore = async () => {
         await replyQuery.fetchNextPage();
     }
 
+    const renderReply = () => {
+        if (replyQuery.data) {
+            if (replyQuery.data.pages.length > 1) {
+                return (
+                    replyQuery.data?.pages[1].content.map((reply) => (
+                        <FeedReplyItem key={reply.id} reply={reply}/>
+                    ))
+                )
+            } else {
+                return (
+                    replyQuery.data?.pages[0].content.map((reply) => (
+                        <FeedReplyItem key={reply.id} reply={reply}/>
+                    ))
+                )
+            }
+        }
+    }
+
     return (
         <div className={style.container}>
             <div className={isModal ? style.reply_main_box : ''}>
-                <div className={isModal ? style.reply_box :""}>
-                    {
-                        replyQuery.data &&
-                        replyQuery.data?.pages.map((page)=> (
-                        page.content.map((reply)=> (
-                            <FeedReplyItem key={reply.id} reply={reply}/>
-                        ))
-                    ))}
+                <div className={isModal ? style.reply_box : ""}>
+                    {renderReply()}
                 </div>
-                {replyQuery.hasNextPage && <span className={style.reply_more_txt} onClick={handleClickMore}>댓글 더보기...</span> }
+                {replyQuery.hasNextPage &&
+                <span className={style.reply_more_txt} onClick={handleClickMore}>댓글 더보기...</span>}
             </div>
             {isModal && <span className={style.blank_reply_more_txt}></span>}
             <div className={isModal ? style.modal_reply_input_box : ''}>
