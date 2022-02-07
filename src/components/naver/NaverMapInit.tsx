37e9971b2520fd7720cbed25cde9ccbd89f6f643
gudwh14 <alxious@naver.com>
2022-02-07 18:13:22 +0900

    feat : issue#619  지역기반 검색 서비스 API 연동

--- src/components/naver/NaverMapInit.tsx
+++ src/components/naver/NaverMapInit.tsx
@@ -1,41 +1,59 @@
 import Script from 'next/script'
 import {useEffect} from "react";
+import {useQuery} from "react-query";
+import {getMap, IMapDto} from "src/apis/Map";
+import {IShopDto} from "src/apis/Shop";
 
-const NaverMapInit = (): JSX.Element => {
-    const clientId = 'tekcukac4f';
+const clientId = 'tekcukac4f';
+
+interface INaverMapInitProps {
+    regCode: number
+    shopList: IShopDto[]
+}
+
+const NaverMapInit = ({regCode, shopList}: INaverMapInitProps): JSX.Element => {
+    const mapQuery = useQuery<IMapDto>(['map', regCode], () => getMap(regCode));
 
     const naverCallback = async () => {
-        console.log('naverCallback');
         const map = new naver.maps.Map('map', {
-            center: new naver.maps.LatLng(37.3089444, 127.0025879),
-            zoom: 15
+            center: new naver.maps.LatLng(37.2787794, 126.95581),
+            zoom: 12
         })
 
-        const marker = new naver.maps.Marker({
-            position: new naver.maps.LatLng(37.3089444, 127.0025879),
-            map: map
-        })
-
-        const infoWindow = new naver.maps.InfoWindow({
-            content: '<div class="marker_box">' +
-                '<div class="marker_info_box">' +
-                    '<span class="marker_shop_name_txt">아일랜드골프 수원 인계점</span>' +
-                    '<span class="marker_shop_sub_txt">수원시 장안구 조원동 16-9</span>' +
-                    '<button class="marker_btn">스토어 이동</button>' +
-                '</div>' +
-                '<img class="marker_shop_img" src="golfShop_img2.jpeg"/>' +
-                '</div>',
-        });
-
-        naver.maps.Event.addListener(marker, 'click', () => {
-            infoWindow.getMap() ? infoWindow.close() : infoWindow.open(map, marker)
-        })
+        const createMarker = () => {
+            shopList.map((shop) => {
+                const marker = new naver.maps.Marker({
+                    position: new naver.maps.LatLng(shop.longitude, shop.latitude),
+                    map: map
+                });
+                const infoWindow = new naver.maps.InfoWindow({
+                    content: '<div class="marker_box">' +
+                        '<div class="marker_info_box">' +
+                        '<span class="marker_shop_name_txt">' + shop.shopName + '</span>' +
+                        '<span class="marker_shop_sub_txt">' + '⭐️ 4.5 (100)' + '</span>' +
+                        '<span class="marker_shop_sub_txt">' + shop.location + '</span>' +
+                        '<span class="marker_shop_sub_txt">' + shop.subLocation + '</span>' +
+                        '</div>' +
+                        '<img class="marker_shop_img" src=' + shop.imgSrc + '/>' +
+                        '</div>',
+                });
+                naver.maps.Event.addListener(marker, 'click', () => {
+                    infoWindow.getMap() ? infoWindow.close() : infoWindow.open(map, marker)
+                });
+            })
+        }
+        createMarker();
     }
 
     useEffect(() => {
         if (typeof naver != "undefined")
             naver && naverCallback();
-    },[])
+    }, [])
+
+    useEffect(() => {
+        if (typeof naver != "undefined")
+            naver && naverCallback();
+    }, [shopList])
 
     return (
         <div>
