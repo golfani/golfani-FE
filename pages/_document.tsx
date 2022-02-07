f1909d5ee24db8f54d0cba4a3f95129027b17e8e
gudwh14 <alxious@naver.com>
2022-02-07 18:15:25 +0900

    feat : issue#620 favicon 구현

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript,DocumentContext } from 'next/document'
-
 /**
  * Next.js 제공하는 Document 를 커스텀마이즈 하여 사용 할 수 있다.
  * Next.js 페이즈들은 마크업 정의 건너띄고 페이지 들을 만들기 때문에
@@ -15,7 +14,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="ko">
-                <Head/>
+                <Head>
+                    <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon.ico"/>
+                </Head>
                 <body>
                     <Main/>
                     <NextScript/>
