7ed0f4fb6e65a2ab814423d3ac7fc159dced38cb
gudwh14 <alxious@naver.com>
2022-02-18 02:17:47 +0900

    fix : Chat Mount, UnMount bug fix

--- pages/message/[roomId].tsx
+++ pages/message/[roomId].tsx
@@ -56,7 +56,7 @@ const Message = (): JSX.Element => {
         }
         onMount();
         return () => unMount();
-    }, []);
+    }, [roomId]);
 
     return (
         <div className={style.container}>
