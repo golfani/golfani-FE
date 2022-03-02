d25fe818267efd27cbd2afb2ae0c3784ec428b58
gudwh14 <alxious@naver.com>
2022-03-02 22:38:29 +0900

    feat : issue#642  프로필 이미지 변경시, 프로필 이미지 새로고침

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -6,27 +6,29 @@ import {dataURLtoFile} from "src/utils/fileUtil";
 import {IProfileMemberProps} from "../../../pages/profile/[userId]";
 import MessageSendModal from "src/components/modals/MessageSendModal";
 import {handleClickRefOutSide} from "src/utils/clickUtil";
+import {useRouter} from "next/router";
 
-const UserProfile = ({member} : IProfileMemberProps) : JSX.Element => {
+const UserProfile = ({member}: IProfileMemberProps): JSX.Element => {
     const userId = getCookie('userId');
     const hasRole = userId === member.userId;
     const [openSendMsg, setOpenSendMsg] = useState(false);
     const targetRef = useRef<HTMLDivElement>(null);
+    const router = useRouter();
 
     const onCloseSendMsg = () => {
         setOpenSendMsg(false);
     }
 
     const handleClickMsgButton = () => {
-        setOpenSendMsg((openSendMsg)=>!openSendMsg);
+        setOpenSendMsg((openSendMsg) => !openSendMsg);
     }
 
-    const handleChangeImage = (event : ChangeEvent) => {
+    const handleChangeImage = (event: ChangeEvent) => {
         const input = event.target as HTMLInputElement;
-        if(!input.files?.length) {
+        if (!input.files?.length) {
             return;
         }
-        const file : File = input.files[0];
+        const file: File = input.files[0];
         const fileUrl = URL.createObjectURL(file);
         let canvas = window.document.createElement('canvas');
         let ctx = canvas.getContext('2d');
@@ -38,43 +40,45 @@ const UserProfile = ({member} : IProfileMemberProps) : JSX.Element => {
         canvasImage.onload = async () => {
             let width = canvasImage.width;
             let height = canvasImage.height;
-            if(width> height) {
-                if(width > maxWidth) {
+            if (width > height) {
+                if (width > maxWidth) {
                     height = height * maxWidth / width;
                     width = maxWidth;
                 }
             } else {
-                if(height > maxHeight) {
+                if (height > maxHeight) {
                     width = width * maxHeight / height;
                     height = maxHeight;
                 }
             }
             canvas.width = width;
             canvas.height = height;
-            ctx?.drawImage(canvasImage, 0 ,0, width, height);
+            ctx?.drawImage(canvasImage, 0, 0, width, height);
             const resizeImageUrl = canvas.toDataURL(file.type, 0.75);
-            const resizeImageFile = dataURLtoFile(resizeImageUrl,file.name);
+            const resizeImageFile = dataURLtoFile(resizeImageUrl, file.name);
 
             try {
                 const response = await registerProfileImage(resizeImageFile);
-            }
-            catch (e) {
+                router.reload();
+            } catch (e) {
             }
         }
     }
 
-    handleClickRefOutSide(targetRef,onCloseSendMsg);
+    handleClickRefOutSide(targetRef, onCloseSendMsg);
 
     return (
         <div className={style.container}>
             <div className={style.profile_box}>
-                <img src={getProfileImage(member.userId,"HIGH")}
-                           width={150}
-                           height={150}
-                           className={style.img}
+                <img src={getProfileImage(member.userId, "HIGH")}
+                     alt={'profile'}
+                     width={150}
+                     height={150}
+                     className={style.img}
                 />
-                {hasRole && <label htmlFor={'input'} className={style.profile_img_btn}>프로필 사진 변경</label> }
-                <input id={'input'} type='file' accept={'image/jpeg, image/png'} className={style.hidden} onChange={handleChangeImage}/>
+                {hasRole && <label htmlFor={'input'} className={style.profile_img_btn}>프로필 사진 변경</label>}
+                <input id={'input'} type='file' accept={'image/jpeg, image/png'} className={style.hidden}
+                       onChange={handleChangeImage}/>
                 <span className={style.user_txt}>{member.userId}</span>
             </div>
             <div className={style.user_info_box}>
@@ -82,8 +86,8 @@ const UserProfile = ({member} : IProfileMemberProps) : JSX.Element => {
                 {/*<Achievements/>*/}
             </div>
             <div className={style.user_menu_box} ref={targetRef}>
-                {hasRole || <button className={style.msg_btn} onClick={handleClickMsgButton}>메세지 보내기</button> }
-                {openSendMsg && <MessageSendModal member={member} setModalOpen={setOpenSendMsg}/> }
+                {hasRole || <button className={style.msg_btn} onClick={handleClickMsgButton}>메세지 보내기</button>}
+                {openSendMsg && <MessageSendModal member={member} setModalOpen={setOpenSendMsg}/>}
             </div>
         </div>
     );
