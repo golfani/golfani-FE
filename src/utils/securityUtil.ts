c22e8096fa0ee5813f29fdcafedfebf8332ac195
gudwh14 <alxious@naver.com>
2022-03-02 22:31:00 +0900

    style : code style change

--- src/utils/securityUtil.ts
+++ src/utils/securityUtil.ts
@@ -3,23 +3,22 @@ import {securityAxios} from "../security/axios";
 import {removeCookie} from "./cookieUtil";
 import {socket} from "../socket/socket";
 
-const onRegenerateAccessToken = async (userId : string) => {
+const onRegenerateAccessToken = async (userId: string) => {
     try {
         const response = await regenerateAccessToken(userId);
         securityAxios.defaults.headers.common['Authorization'] = `Bearer ${response.data}`;
         socket.socketClient.connectHeaders['Authorization'] = response.data;
-    }
-    catch (e) {
+    } catch (e) {
         removeCookie('userId');
         window.location.href = 'login';
         alert('로그인 세션 만료, 다시 로그인 해주세요');
     }
 }
 
-export const onSilentRefresh = async (userId : string) => {
+export const onSilentRefresh = async (userId: string) => {
     const ACCESS_TOKEN_EXPIRE = 1000 * 60 * 15; // ms
     await onRegenerateAccessToken(userId);
-    setTimeout(async ()=> {
+    setTimeout(async () => {
         await onSilentRefresh(userId);
-    },ACCESS_TOKEN_EXPIRE - 6000 * 5); // ms
+    }, ACCESS_TOKEN_EXPIRE - 6000 * 5); // ms
 }
