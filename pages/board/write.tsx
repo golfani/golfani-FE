1f818578c3e9d31be9b2e4fbe5e740d5cebfd2f1
gudwh14 <alxious@naver.com>
2022-03-08 10:26:08 +0900

    feat : 글쓰기 페이지 진입시 로그인한 유저인지 체크 로직 추가 (#660)

--- pages/board/write.tsx
+++ pages/board/write.tsx
@@ -1,9 +1,21 @@
 import Navbar from "src/components/common/navbar/Navbar";
 import BoardWrite from 'src/components/board/write/BoardWrite';
 import Head from "next/head";
+import {useEffect} from "react";
+import {getCookie} from "src/utils/cookieUtil";
+import {useRouter} from "next/router";
+
 
 const write = (): JSX.Element => {
-    return(
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!getCookie('userId')) {
+            router.push('/login');
+        }
+    }, []);
+
+    return (
         <div>
             <Head>
                 <title>골아니 커뮤니티 - 게시글 작성</title>
