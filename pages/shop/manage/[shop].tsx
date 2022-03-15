5f563cbcd01c314bce00715092bdb38c6676cf24
gudwh14 <alxious@naver.com>
2022-03-15 11:50:16 +0900

    feat : Dynamic import를 이용하여 초기 로딩시간 줄이기 (#672)

--- pages/shop/manage/[shop].tsx
+++ pages/shop/manage/[shop].tsx
@@ -2,7 +2,6 @@ import Navbar from "src/components/common/navbar/Navbar";
 import ShopInfo from "src/components/shop/manage/ShopInfo";
 import ShopManageMenu from "src/components/shop/manage/ShopManageMenu";
 import ShopNotice from "src/components/shop/manage/ShopNotice";
-import ShopItemManage from "../../../src/components/shop/manage/item/ShopItemManage";
 
 const ShopManagePage = () : JSX.Element =>{
     return(
