c383526b120287283915860db01c1c4beb7e154d
gudwh14 <alxious@naver.com>
2022-03-16 11:49:22 +0900

    feat : 파이어베이스 서비스워커 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,10 +4,11 @@ importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = firebase.initializeApp(
+console.log(JSON.parse(new URL(location).searchParams.get("firebaseConfig")))
+firebase.initializeApp(
     JSON.parse(new URL(location).searchParams.get("firebaseConfig"))
 );
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging(firebaseApp);
+const messaging = firebase.messaging();
