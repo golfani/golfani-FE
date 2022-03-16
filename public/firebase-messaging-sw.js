a3bf0d240f4e286c7c0e7ae01bba93efb969e775
gudwh14 <alxious@naver.com>
2022-03-16 11:16:02 +0900

    fix : 파이어베이스 서비스워커 정상적으로 등록안되는 버그 수정 (#676)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,15 +4,9 @@ importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FCM_APP_KEY,
-    authDomain: "golfani.firebaseapp.com",
-    projectId: "golfani",
-    storageBucket: "golfani.appspot.com",
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FCM_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-});
+const firebaseApp = firebase.initializeApp(
+    JSON.parse(new URL(location).searchParams.get("firebaseConfig"))
+);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
